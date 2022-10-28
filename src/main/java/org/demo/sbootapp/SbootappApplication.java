d9b8be683621361beb9684165f6e17dae4ad288b
Sola Otudeko <so2d@icloud.com>
2022-10-28 16:18:09 +0100

    1

---
+++ src/main/java/org/demo/sbootapp/SbootappApplication.java
@@ -0,0 +1,13 @@
+package org.demo.sbootapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbootappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbootappApplication.class, args);
+	}
+
+}
